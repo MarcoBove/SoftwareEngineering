ef933c07b1f0a94fd55683420614cc65371de877
Andre <Andre@Andrea_PC>
2023-11-22 17:44:30 +0100

    Second commit, pages design fxml + empty class "Action,Trigger" creation

---
+++ Project_SE_Gruppo04/src/test/RuleTest.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package test;
+import model.Action;
+
+import org.junit.*;
+import static org.junit.Assert.*;
+/**
+ *
+ * @author Andre
+ */
+public class RuleTest {
+    
+}
