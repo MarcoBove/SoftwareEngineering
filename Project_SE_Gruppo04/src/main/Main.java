feb634c1f62686f0464ac009e8ea7b78fdbfb006
Andre <Andre@Andrea_PC>
2023-11-23 12:51:32 +0100

    Added Rule, RuleTest, changed name of xml file

--- Project_SE_Gruppo04/src/main/Main.java
+++ Project_SE_Gruppo04/src/main/Main.java
@@ -2,23 +2,22 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXML.java to edit this template
  */
-package view;
+package main;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-
 /**
  *
  * @author Andre
  */
-public class Project_SE_Gruppo04 extends Application {
+public class Main extends Application {
     
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("homePage.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/view/homePage.fxml"));
         
         Scene scene = new Scene(root);
         stage.setScene(scene);
